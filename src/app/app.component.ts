01fbc273ae0fcd7717da708448190cbe7f786cb6
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-11-15 05:32:39 -0300

    feat(app): set base structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, MatSlideToggleModule, MatDividerModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withViewTransitions,
+} from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions(), withComponentInputBinding()),
     provideAnimations(),
     provideClientHydration(),
   ],
