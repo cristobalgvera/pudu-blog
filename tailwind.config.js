9af80b5684215cbdd37763fbd315ebeb3955cf52
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-11-15 05:24:01 -0300

    feat(style): set colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
-  corePlugins: {
-    preflight: false, // To integrate properly with Angular Material
+  darkMode: 'selector',
+  theme: {
+    colors: {
+      primary: 'hsl(var(--color-primary))',
+      secondary: 'hsl(var(--color-secondary))',
+      tertiary: 'hsl(var(--color-tertiary))',
+      'font-color': 'hsl(var(--color-font))',
+      white: 'hsl(var(--color-white))',
+      black: 'hsl(var(--color-black))',
+      current: 'currentColor',
+      transparent: 'transparent',
+    },
   },
 };
